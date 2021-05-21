5af5f648590ed21a6adee7f6d8e4175482e05ded
pelmenstar1 <pelmenstar1@gmail.com>
2021-05-21 23:51:09 +0300

    Use kotlin.test in shared.android module

---
+++ shared/android/build/generated/source/buildConfig/androidTest/debug/com/pelmenstar/projktSens/shared/android/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pelmenstar.projktSens.shared.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.pelmenstar.projktSens.shared.android.test";
+  public static final String BUILD_TYPE = "debug";
+}
