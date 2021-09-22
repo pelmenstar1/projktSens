5f97d6d1a4df545b0f622c00a7d0b5b3383849d3
O. Khmaruk <pelmenstar1@gmail.com>
2021-09-22 19:39:35 +0300

    Change impl of NetworkUtils.isConnectedToAnyNetwork

--- shared/android/src/main/java/com/pelmenstar/projktSens/shared/android/NetworkUtils.java
+++ shared/android/src/main/java/com/pelmenstar/projktSens/shared/android/NetworkUtils.java
@@ -2,7 +2,10 @@ package com.pelmenstar.projktSens.shared.android;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -11,8 +14,23 @@ public final class NetworkUtils {
 
     public static boolean isConnectedToAnyNetwork(@NotNull Context context) {
         ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo active = manager.getActiveNetworkInfo();
 
-        return active != null && active.isConnected();
+        if (Build.VERSION.SDK_INT >= 23) {
+            Network active = manager.getActiveNetwork();
+
+            if(active != null) {
+                NetworkCapabilities capabilities = manager.getNetworkCapabilities(active);
+
+                if(capabilities != null) {
+                    return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+                }
+            }
+
+            return false;
+        } else {
+            NetworkInfo active = manager.getActiveNetworkInfo();
+
+            return active != null && active.isConnected();
+        }
     }
 }
