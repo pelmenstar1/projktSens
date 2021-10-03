0dcce2cfa5a90c0c014560a12691272b8222f10e
O. Khmaruk <pelmenstar1@gmail.com>
2021-10-03 23:25:16 +0300

    Add async way to ServerAvailabilityProvider and add tests

---
+++ jserver/src/androidTest/java/com/pelmenstar/projktSens/jserver/ServerAvailProviderTests.kt
@@ -0,0 +1,47 @@
+package com.pelmenstar.projktSens.jserver
+
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.pelmenstar.projktSens.jserver.di.DaggerAppComponent
+import com.pelmenstar.projktSens.serverProtocol.ServerAvailabilityProvider
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.runner.RunWith
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@RunWith(AndroidJUnit4::class)
+class ServerAvailProviderTests {
+    private val context = InstrumentationRegistry.getInstrumentation().context
+    private val component = DaggerAppComponent.builder().appModule(TestAppModule(context)).build()
+    private val server = component.server()
+
+    @Test
+    fun isAvailableTest() {
+        fun testCase(forceBlocking: Boolean) {
+            runBlocking {
+                val provider = ServerAvailabilityProvider(component.protoConfig(), forceBlocking)
+
+                server.stop()
+                assertFalse(provider.isAvailable(), "forceBlocking=$forceBlocking")
+
+                server.startOnNewThread()
+                delay(3000) // wait until server actually starts
+
+                assertTrue(provider.isAvailable(), "forceBlocking=$forceBlocking")
+
+                server.stop()
+            }
+        }
+
+        testCase(forceBlocking = true)
+
+        // There's no need to test async way if SDK int < 26
+        if(Build.VERSION.SDK_INT >= 26) {
+            testCase(forceBlocking = false)
+        }
+    }
+}
--- serverProtocol/src/main/java/com/pelmenstar/projktSens/serverProtocol/ServerAvailabilityProvider.kt
+++ serverProtocol/src/main/java/com/pelmenstar/projktSens/serverProtocol/ServerAvailabilityProvider.kt
@@ -1,17 +1,57 @@
 package com.pelmenstar.projktSens.serverProtocol
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.pelmenstar.projktSens.shared.connectSuspend
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 import java.net.Socket
+import java.nio.channels.AsynchronousChannelGroup
+import java.nio.channels.AsynchronousSocketChannel
 
-class ServerAvailabilityProvider(config: ProtoConfig) {
+class ServerAvailabilityProvider(
+    config: ProtoConfig,
+    private var forceBlocking: Boolean = false
+) {
     private val address = config.socketAddress
 
+    private var isAvailableAsyncHandler: (suspend CoroutineScope.() -> Boolean)? = null
+
+    init {
+        if(Build.VERSION.SDK_INT >= 26) {
+            isAvailableAsyncHandler = {
+                AsynchronousSocketChannel.open().use { channel ->
+                    channel.connectSuspend(address)
+                    true
+                }
+            }
+        }
+    }
+
     suspend fun isAvailable(): Boolean {
+        return if(Build.VERSION.SDK_INT >= 26 && !forceBlocking) {
+            isAvailableAsync()
+        } else {
+            isAvailableBlocking()
+        }
+    }
+
+    private suspend fun isAvailableBlocking(): Boolean {
         return try {
             val socket = Socket()
             socket.connectSuspend(address, 5000)
 
             true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    @RequiresApi(26)
+    private suspend fun isAvailableAsync(): Boolean {
+        return try {
+            withTimeout(5000L, isAvailableAsyncHandler!!)
         } catch (e: Exception) {
             false
         }
