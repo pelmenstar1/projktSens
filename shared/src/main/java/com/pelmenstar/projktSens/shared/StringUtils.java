36fd5296839bb1da2fce66a026af49ebec4e9dd1
O. Khmaruk <pelmenstar1@gmail.com>
2021-10-06 20:30:01 +0300

    Optimize StringUtils

--- shared/src/main/java/com/pelmenstar/projktSens/shared/StringUtils.java
+++ shared/src/main/java/com/pelmenstar/projktSens/shared/StringUtils.java
@@ -9,30 +9,19 @@ import java.util.Arrays;
  * Represents a helper class that efficiently build strings
  */
 public final class StringUtils {
-    private static final byte[] DIGITS_TENS = {
-            '0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-            '1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
-            '2', '2', '2', '2', '2', '2', '2', '2', '2', '2',
-            '3', '3', '3', '3', '3', '3', '3', '3', '3', '3',
-            '4', '4', '4', '4', '4', '4', '4', '4', '4', '4',
-            '5', '5', '5', '5', '5', '5', '5', '5', '5', '5',
-            '6', '6', '6', '6', '6', '6', '6', '6', '6', '6',
-            '7', '7', '7', '7', '7', '7', '7', '7', '7', '7',
-            '8', '8', '8', '8', '8', '8', '8', '8', '8', '8',
-            '9', '9', '9', '9', '9', '9', '9', '9', '9', '9',
-    };
-
-    private static final byte[] DIGIT_ONES = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    private static final byte[] DIGITS = {
+            0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0x10,
+            0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x20,
+            0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30,
+            0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x40,
+            0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x50,
+            0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x60,
+            0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x70,
+            0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0xffffff80,
+            0xffffff81, 0xffffff82, 0xffffff83, 0xffffff84, 0xffffff85, 0xffffff86,
+            0xffffff87, 0xffffff88, 0xffffff89, 0xffffff90, 0xffffff91, 0xffffff92,
+            0xffffff93, 0xffffff94, 0xffffff95, 0xffffff96, 0xffffff97, 0xffffff98,
+            0xffffff99,
     };
 
     private static final char[] hexBuffer = new char[8];
@@ -260,13 +249,21 @@ public final class StringUtils {
     }
 
     public static void writeTwoDigits(char @NotNull [] buffer, int offset, int number) {
-        buffer[offset] = (char) DIGITS_TENS[number];
-        buffer[offset + 1] = (char) DIGIT_ONES[number];
+        int tenOne = DIGITS[number] & 0xFF;
+        int ten = tenOne >> 4;
+        int one = tenOne & 0xF;
+
+        buffer[offset] = (char)('0' + ten);
+        buffer[offset + 1] = (char)('0' + one);
     }
 
     private static void appendTwoDigitsInternal(int number, @NotNull StringBuilder sb) {
-        sb.append((char) DIGITS_TENS[number]);
-        sb.append((char) DIGIT_ONES[number]);
+        int tenOne = DIGITS[number] & 0xFF;
+        int ten = tenOne >> 4;
+        int one = tenOne & 16;
+
+        sb.append((char)('0' + ten));
+        sb.append((char)('0' + one));
     }
 
     private static void writeThreeDigits(char @NotNull [] buffer, int offset, int number) {
