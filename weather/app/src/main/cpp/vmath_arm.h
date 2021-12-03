e63386a68302c322b3677a91450c8bb7e982b0c9
pelme <pelmenstar1@gmail.com>
2021-12-03 20:40:19 +0200

    Delete unused v_acosf

--- weather/app/src/main/cpp/vmath_arm.h
+++ weather/app/src/main/cpp/vmath_arm.h
@@ -13,7 +13,6 @@ float32x2_t v_asinf(float32x2_t x);
 float32x2_t v_sinf(float32x2_t x);
 float32x4_t v_sinfq(float32x4_t x);
 
-float32x2_t v_acosf(float32x2_t x);
 float32x2_t v_cosf(float32x2_t x);
 
 float32x2_t v_sincosf_fast(float x);
