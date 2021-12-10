a30d847e348a3dca8b95a52922217cac23975e29
pelme <pelmenstar1@gmail.com>
2021-12-10 21:37:12 +0200

    Fix server tests

--- jserver/src/androidTest/java/com/pelmenstar/projktSens/jserver/TestAppModule.kt
+++ jserver/src/androidTest/java/com/pelmenstar/projktSens/jserver/TestAppModule.kt
@@ -7,18 +7,15 @@ import com.pelmenstar.projktSens.jserver.logging.LogLevel
 import com.pelmenstar.projktSens.jserver.logging.LoggerConfig
 import com.pelmenstar.projktSens.jserver.repo.DbServerWeatherRepository
 import com.pelmenstar.projktSens.serverProtocol.ProtoConfig
-import com.pelmenstar.projktSens.serverProtocol.ProtoConfigImpl
 import com.pelmenstar.projktSens.serverProtocol.RawContract
-import com.pelmenstar.projktSens.weather.models.WeatherInfoProvider
 import com.pelmenstar.projktSens.weather.models.WeatherRepository
-import java.net.InetAddress
 import java.net.InetSocketAddress
 
 class TestAppModule(context: Context): AppModule(context) {
     private val weatherRepo = DbServerWeatherRepository.inMemory(context)
 
     override fun protoConfig(): ProtoConfig {
-        return ProtoConfigImpl(
+        return ProtoConfig(
             InetSocketAddress(host(), 10001),
             10000,
             RawContract
